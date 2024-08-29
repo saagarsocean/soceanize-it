25973396b3b504dfebbca97a65431957fe794c1a
saagarsocean <91834645+saagarsocean@users.noreply.github.com>
2024-08-30 02:03:35 +0530

    Children updated

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -3,6 +3,7 @@ import { Box, Flex } from "@chakra-ui/react";
 
 interface PageContentLayoutProps {
   maxWidth?: string;
+  children?: React.ReactNode[]
 }
 
 // Assumes array of two children are passed
@@ -18,7 +19,7 @@ const PageContentLayout: React.FC<PageContentLayoutProps> = ({
           width={{ base: "100%", md: "65%" }}
           mr={{ base: 0, md: 6 }}
         >
-          {children && children[0 as keyof typeof children]}
+          {Array.isArray(children) && children[0]}
         </Flex>
         {/* Right Content */}
         <Box
@@ -26,7 +27,7 @@ const PageContentLayout: React.FC<PageContentLayoutProps> = ({
           flexDirection="column"
           flexGrow={1}
         >
-          {children && children[1 as keyof typeof children]}
+          {Array.isArray(children) && children[1]}
         </Box>
       </Flex>
     </Flex>
